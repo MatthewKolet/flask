66853debb02e0290ec1c0cb94595d4794ad67a95
Matthew Kolet <matthew-kolet@uiowa.edu>
2022-04-20 22:36:15 -0500

    adding tests

---
+++ tests/unit/test_web.py
@@ -0,0 +1,36 @@
+import time
+
+def test_index_route(app, client):
+
+    print("\r")
+    print(" -- / GET test")
+    with app.test_client() as test_client:
+        res = test_client.get('/')
+        assert res.status_code == 200
+        assert b"Welcome to VTM!" in res.data
+
+
+def test_mike_route(app, client):
+  
+    print("-- /about GET test")
+    with app.test_client() as test_client:
+        res = test_client.get('/about')
+        assert res.status_code == 200
+        assert b"About Vertical Tank Maintenance" in res.data
+
+
+def test_estimate_route(app, client):
+
+    print("-- /estimate GET test")
+    with app.test_client() as test_client:
+        res = test_client.get('/estimate')
+        assert res.status_code == 302
+        assert res.headers["Location"] == "/estimate"
+
+def test_estimate_functionality(app, client):
+
+    print("-- /estimate POST test")
+    with app.test_client() as test_client:
+        size = {"radius":180, "height":360}
+        res = test_client.post('estimate', 141300.0)
+        assert res.status_code == 200 
