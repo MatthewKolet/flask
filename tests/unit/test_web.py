6b461682efa5458e55447632ed2268809afe0245
Matthew Kolet <matthew-kolet@uiowa.edu>
2022-04-24 19:51:08 -0500

    final adjustments to test files

--- tests/unit/test_web.py
+++ tests/unit/test_web.py
@@ -10,7 +10,7 @@ def test_index_route(app, client):
         assert b"Welcome to VTM!" in res.data
 
 
-def test_mike_route(app, client):
+def test_about_route(app, client):
   
     print("-- /about GET test")
     with app.test_client() as test_client:
@@ -24,13 +24,13 @@ def test_estimate_route(app, client):
     print("-- /estimate GET test")
     with app.test_client() as test_client:
         res = test_client.get('/estimate')
-        assert res.status_code == 302
-        assert res.headers["Location"] == "/estimate"
+        assert res.status_code == 200
+        #assert res.headers["Location"] == "/estimate"
 
 def test_estimate_functionality(app, client):
 
     print("-- /estimate POST test")
     with app.test_client() as test_client:
         size = {"radius":180, "height":360}
-        res = test_client.post('estimate', 141300.0)
-        assert res.status_code == 200 
+        res = test_client.post('estimate', data=size)
+        assert res.status_code == 200
